a8ea88a2e8aa6407678b04cd75478b222a3b1480
anasayubi <anasayubi@hotmail.com>
2018-04-23 13:26:31 +0500

    add -l or --list parameter skeleton and exception handling for undefined params

--- op.cpp
+++ op.cpp
@@ -3,5 +3,24 @@
 
 using namespace std;
 int main(int argc, char *argv[]){
-  cout << "hello world!" << endl;
+
+  cxxopts::Options options("guiter", "Frictionless Terminal to GUI Interaction");
+  options.add_options()
+  ("l,list", "Enable debugging");
+  
+  try{ 
+    auto result = options.parse(argc, argv);
+
+    if(result.count("list") == 1)
+      cout << result["list"].as<bool>() << endl;
+
+  } catch(cxxopts::option_not_exists_exception e){
+    string const err_str = e.what(); 
+    int first_space = err_str.find(' ');
+    int second_space = err_str.find(' ', first_space + 1);
+    string parameter_str = err_str.substr(first_space + 1, second_space - first_space - 1);
+    cout << "Invalid parameter: " << parameter_str << endl; 
+  }
+    
+
 }

524bbf32c99989ab5d7e28ad0d16d1b1d0e8138a
anasayubi <anasayubi@hotmail.com>
2018-04-27 11:12:15 +0500

    refactor cmdline options to use boost lib

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 main: libs/helper.cpp libs/helper.hpp op.cpp
-	g++ -o op op.cpp libs/helper.cpp -I libs/
+	g++ -o op op.cpp libs/helper.cpp /usr/lib/libboost_program_options.a -I libs/
 
 comp-tests: tests/test_get_token_from_str.cpp libs/helper.cpp
 	g++ -o test ./tests/test_get_token_from_str.cpp ./libs/helper.cpp -I libs/
--- op.cpp
+++ op.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
-#include <cxxopts.hpp>
+//#include <cxxopts.hpp>
+#include "boost/program_options.hpp"
 #include "helper.hpp"
 using namespace std;
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[]){
+  // desc = description
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("list,l", "list the current processes running background")
+    ("help,h", "set compression level");
 
-  cxxopts::Options options("guiter", "Frictionless Terminal to GUI Interaction");
-  options.add_options()
-    ("l,list", "Enable debugging");
+  // vm = variables map
+  po::variables_map vm;
 
-  try{ 
-    auto result = options.parse(argc, argv);
+  // try{ 
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);   
 
-    if(result.count("list") == 1)
-      cout << result["list"].as<bool>() << endl;
+    if (vm.count("help")) {
+      cout << desc << "\n";
+      return 1;
+    }
+
+    if (vm.count("list")) {
+      // cout << desc << "\n";
+      cout << "list running" << endl;
+      return 1;
+    }
+    // if(result.count("list") == 1)
+    //   cout << result["list"].as<bool>() << endl;
       
 
-  } catch(cxxopts::option_not_exists_exception e){
-    string const err_str = e.what(); 
-    cout << get_token_from_str(err_str, " ", 2) << endl;
-    // int first_space = err_str.find(' ');
-    // int second_space = err_str.find(' ', first_space + 1);
-    // string parameter_str = err_str.substr(first_space + 1, second_space - first_space - 1);
-    // cout << "Invalid parameter: " << parameter_str << endl; 
-  }
+    //} // catch(cxxopts::option_not_exists_exception e){
+  //   string const err_str = e.what(); 
+  //   cout << get_token_from_str(err_str, " ", 2) << endl;
+  //   // int first_space = err_str.find(' ');
+  //   // int second_space = err_str.find(' ', first_space + 1);
+  //   // string parameter_str = err_str.substr(first_space + 1, second_space - first_space - 1);
+  //   // cout << "Invalid parameter: " << parameter_str << endl; 
+  // }
     
 
 }
